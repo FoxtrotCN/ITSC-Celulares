f2b71d3a0d2d485caa429ebbd982cadeadbad3b4
Fernando <fernandostarlin1012@gmail.com>
2024-07-12 17:48:20 -0400

    Running first migrations

--- itsc_celulares/models.py
+++ itsc_celulares/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 
@@ -12,7 +13,15 @@ class Technician(models.Model):
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
-        return self.username
+        return self.first_name
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100, null=False, blank=False)
+    last_name = models.CharField(max_length=100, null=False, blank=False)
+    primary_phone_number = models.CharField(max_length=20, null=False, blank=False)
+    secondary_phone_number = models.CharField(max_length=20, null=False, blank=True)
+    email = models.EmailField()
 
 
 class CellPhone(models.Model):
@@ -20,10 +29,29 @@ class CellPhone(models.Model):
     model = models.CharField(max_length=100, null=False, blank=False)
     serial_number = models.CharField(max_length=100, null=False, blank=False)
     problem_description = models.TextField(null=False, blank=False)
-    repair_technician = models.ForeignKey(Technician, on_delete=models.DO_NOTHING, related_name="repaired_phones")
-    diagnosis = models.TextField(null=False, blank=False)
+    diagnosis = models.TextField(null=False, blank=True)
+    customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.brand
 
 
+class RepairOrder(models.Model):
+    RECEIVED = "RECEIVED"
+    IN_DIAGNOSIS = "IN DIAGNOSIS"
+    IN_REPAIR = "IN REPAIR"
+    READY_FOR_DELIVERY = "READY FOR DELIVERY"
+    DELIVERED = "DELIVERED"
+
+    REPAIR_ORDER_STATUS = [
+        (RECEIVED, "Recibido"),
+        (IN_DIAGNOSIS, "En Diagnostico"),
+        (IN_REPAIR, "En Reparacion"),
+        (READY_FOR_DELIVERY, "Listo para entrega"),
+        (DELIVERED, "Entregado")
+    ]
+
+    technician = models.ForeignKey(Technician, on_delete=models.DO_NOTHING)
+    device = models.ForeignKey(CellPhone, on_delete=models.DO_NOTHING)
+    date_created = models.DateTimeField(auto_now_add=True)
+    order_status = models.CharField(max_length=100, choices=REPAIR_ORDER_STATUS, default=RECEIVED)
