08a203579cb8b0b9b2d2d85644da37b074f6b243
Fernando <fernandostarlin1012@gmail.com>
2024-07-13 01:15:22 -0400

    Starting to implement the login module and some other templates

---
+++ itsc_celulares/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('login/', views.login_page, name="login"),
+    path('users/', views.users, name="users"),
+]
--- itsc_celulares/views.py
+++ itsc_celulares/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
+
+def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    return render(request, "itsc_celulares/login.html")
+
+
+def home(request):
+    return render(request, "itsc_celulares/dashboard.html")
+
+
+def users(request):
+    return render(request, "itsc_celulares/users.html")
